86cc148d7cf449fbc7e2c84a04a7c243960935cc
lavanya <radha,lavi09@gmail.com>
2020-10-30 12:57:50 +0530

    Add project files.

---
+++ Ef_DBFirst_wpfDemo/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ef_DBFirst_wpfDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LavanyaEntities dbCon = new LavanyaEntities();
+            Donar d1 = new Donar();
+            d1.DonarID = 800;
+            d1.Name = "Dummy";
+            d1.BloodGrp = "A+";
+            d1.City = "Hyderabad";
+
+            dbCon.Donars.Add(d1);
+            dbCon.SaveChanges();
+            System.Windows.Forms.MessageBox.Show("Record Added...");
+        }
+    }
+}
